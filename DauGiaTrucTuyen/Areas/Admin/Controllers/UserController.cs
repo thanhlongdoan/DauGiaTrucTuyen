f15caf715ddaf6e4046c3945346e3082974dea19
long đoàn <doanthanhlong1801@gmail.com>
2019-02-26 08:30:51 +0700

    commit design giao dien trang amdin + list user + detail user

---
+++ DauGiaTrucTuyen/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using DauGiaTrucTuyen.Areas.Admin.Models.ManagerUserViewModel;
+using DauGiaTrucTuyen.Models;
+using System.Linq;
+using System.Web.Mvc;
+using AutoMapper;
+
+namespace DauGiaTrucTuyen.Areas.Admin.Controllers
+{
+    public class UserController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Admin/User
+        public ActionResult Index()
+        {
+            return View(db.Users.ToList());
+        }
+        public ActionResult Update()
+        {
+            return View();
+        }
+        public ActionResult Detail(string id)
+        {
+            var item = db.Users.Find(id);
+
+            if (item != null)
+            {
+                var model = Mapper.Map<DetailUserViewModel>(item);
+                return View(model);
+            }
+            return View("Error");
+        }
+        public ActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
--- DauGiaTrucTuyen/Global.asax.cs
+++ DauGiaTrucTuyen/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DauGiaTrucTuyen.Areas.Admin.Models.ManagerUserViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,10 @@ namespace DauGiaTrucTuyen
     {
         protected void Application_Start()
         {
+            AutoMapper.Mapper.Initialize(conf =>
+            {
+                conf.CreateMap<DetailUserViewModel, Microsoft.AspNet.Identity.EntityFramework.IdentityUser>();
+            });
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
