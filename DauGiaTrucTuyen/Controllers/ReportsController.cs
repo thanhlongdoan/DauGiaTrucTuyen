fa73a1a66cdcaab1f358dfe54b4e294e49441395
Hi-XV <Hi-XV@DESKTOP-8N0O4EO>
2019-04-09 22:10:19 +0700

    commit

---
+++ DauGiaTrucTuyen/Controllers/ReportsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DauGiaTrucTuyen.Data;
+
+namespace DauGiaTrucTuyen.Controllers
+{
+    public class ReportsController : Controller
+    {
+        private Db_DauGiaTrucTuyen db = new Db_DauGiaTrucTuyen();
+
+        // GET: Reports
+      
+        public ActionResult Index()
+        {
+            var reports = db.Reports.Include(r => r.User);
+            return View(reports.ToList());
+        }
+
+        // GET: Reports/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Report report = db.Reports.Find(id);
+            if (report == null)
+            {
+                return HttpNotFound();
+            }
+            return View(report);
+        }
+
+        // GET: Reports/Create
+        public ActionResult Create()
+        {
+            ViewBag.User_Id = new SelectList(db.Users, "Id", "FirstName");
+            return View();
+        }
+
+        // POST: Reports/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Report report)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Reports.Add(report);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.User_Id = new SelectList(db.Users, "Id", "FirstName", report.User_Id);
+            return View(report);
+        }
+
+        // GET: Reports/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Report report = db.Reports.Find(id);
+            if (report == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.User_Id = new SelectList(db.Users, "Id", "FirstName", report.User_Id);
+            return View(report);
+        }
+
+        // POST: Reports/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Reports_Id,Title,Content,CreateDate,CreateBy,Status,User_Id")] Report report)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(report).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.User_Id = new SelectList(db.Users, "Id", "FirstName", report.User_Id);
+            return View(report);
+        }
+
+        // GET: Reports/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Report report = db.Reports.Find(id);
+            if (report == null)
+            {
+                return HttpNotFound();
+            }
+            return View(report);
+        }
+
+        // POST: Reports/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Report report = db.Reports.Find(id);
+            db.Reports.Remove(report);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
