5d2a612c1ee5eaff300d780e2270676daa6dd11c
long đoàn <doanthanhlong1801@gmail.com>
2019-04-12 01:08:01 +0700

    update db

--- Db/dbo/Tables/Transactions.sql
+++ Db/dbo/Tables/Transactions.sql
@@ -1,11 +1,14 @@
 ﻿CREATE TABLE [dbo].[Transactions] (
-    [Transaction_Id] NVARCHAR (128) NOT NULL,
-    [AuctionTime]    TIME (7)       NULL,
-    [AuctionDate]    DATETIME       NULL,
-    [PriceStart]     DECIMAL (18)   NULL,
-    [StepPrice]      INT            NULL,
-    [Product_Id]     NVARCHAR (128) NULL,
+    [Transaction_Id]      NVARCHAR (128) NOT NULL,
+    [TimeLine]            TIME (7)       NULL,
+    [AuctionDateApproved] DATETIME       NULL,
+    [AuctionDateStart]    DATETIME       NULL,
+    [PriceStart]          DECIMAL (18)   NULL,
+    [StepPrice]           INT            NULL,
+    [Product_Id]          NVARCHAR (128) NULL,
     CONSTRAINT [PK_Transaction] PRIMARY KEY CLUSTERED ([Transaction_Id] ASC),
     CONSTRAINT [FK_Transaction_Products] FOREIGN KEY ([Product_Id]) REFERENCES [dbo].[Products] ([Products_Id])
 );
+
+
 
