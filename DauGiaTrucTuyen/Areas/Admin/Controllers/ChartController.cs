6674b0769e6122cdf4d1e76df2b4ba16dc77b6fa
THANH LONG <THANH LONG@DESKTOP-6GT8TGP>
2019-04-18 16:22:31 +0700

    update

---
+++ DauGiaTrucTuyen/Areas/Admin/Controllers/ChartController.cs
@@ -0,0 +1,24 @@
+﻿using DauGiaTrucTuyen.Areas.Admin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DauGiaTrucTuyen.Areas.Admin.Controllers
+{
+    public class ChartController : Controller
+    {
+        // GET: Admin/Chart
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(ChartViewModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ DauGiaTrucTuyen/Areas/Admin/Models/ManagerChartViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DauGiaTrucTuyen.Areas.Admin.Models
+{
+    public class ChartViewModel
+    {
+        public DateTime dateTime { get; set; }
+    }
+}
