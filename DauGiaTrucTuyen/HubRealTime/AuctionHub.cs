70e20a4af255f1f12aa449e82aeade777a3473c2
THANH LONG <THANH LONG@DESKTOP-6GT8TGP>
2019-04-20 15:18:44 +0700

    remove timer in hub

--- DauGiaTrucTuyen/HubRealTime/AuctionHub.cs
+++ DauGiaTrucTuyen/HubRealTime/AuctionHub.cs
@@ -12,11 +12,7 @@ namespace DauGiaTrucTuyen.HubRealTime
     public class AuctionHub : Hub
     {
         Db_DauGiaTrucTuyen db = new Db_DauGiaTrucTuyen();
-        public static bool initialized = false;
-        public static object initLock = new object();
-        static private Timer timer;
         static public Auction auction;
-        public static int secs_10 = 1000;
 
         public AuctionHub()
         {
@@ -51,7 +47,7 @@ namespace DauGiaTrucTuyen.HubRealTime
                 }
             }
         }
-        
+
         public void JoinGroupAuction(string productId)
         {
             var transaction = db.Transactions.Where(x => x.Product_Id == productId && x.Product.StatusProduct.Equals(StatusProduct.Auctioning)).FirstOrDefault();
@@ -59,7 +55,6 @@ namespace DauGiaTrucTuyen.HubRealTime
             if (transaction != null)
             {
                 Groups.Add(Context.ConnectionId, transaction.Transaction_Id);
-                InitializeAuction((DateTime)transaction.AuctionDateStart, transaction.TimeLine.Value.TotalSeconds, transaction.Transaction_Id);
             }
         }
         public void JoinAuction(string productId, string userId, decimal? price)
@@ -89,29 +84,6 @@ namespace DauGiaTrucTuyen.HubRealTime
                 }
                 else
                     Clients.Caller.AuctionError("Giá tiền phải lớn hơn giá tiền hiện tại !");
-            }
-        }
-
-        private void InitializeAuction(DateTime auctionDateStart, double seconds, string transactionId)
-        {
-            auction = new Auction(auctionDateStart.AddSeconds(seconds));
-
-            timer = new Timer(TimerExpired, null, secs_10, 0);
-
-            initialized = true;
-        }
-
-        public void TimerExpired(object state)
-        {
-            if (auction.TimeRemaining > 0)
-            {
-                Clients.All.GetTimeAuction(string.Format("{0:hh\\:mm\\:ss}", auction.GetTimeRemaining()));
-                timer.Change(secs_10, 0);
-            }
-            else
-            {
-                timer.Dispose();
-                Clients.All.GetTimeAuction("End");
             }
         }
     }
