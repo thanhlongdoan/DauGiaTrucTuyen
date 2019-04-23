bc1b06cb313d27a32203629658438a0e41841705
long đoàn <doanthanhlong1801@gmail.com>
2019-04-23 22:31:16 +0700

    commit chart (admin)

--- DauGiaTrucTuyen/Areas/Admin/Controllers/ChartController.cs
+++ DauGiaTrucTuyen/Areas/Admin/Controllers/ChartController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 
 namespace DauGiaTrucTuyen.Areas.Admin.Controllers
@@ -12,7 +13,33 @@ namespace DauGiaTrucTuyen.Areas.Admin.Controllers
         // GET: Admin/Chart
         public ActionResult Index()
         {
+            
             return View();
+        }
+
+        public ActionResult Chart()
+        {
+            decimal Jan = 0;
+            decimal Feb = 4;
+            decimal Mar = 3;
+            decimal Apr = 8;
+            decimal May = 10;
+            decimal Jun = 5;
+            decimal Jul = 3;
+            decimal Aug = 7;
+            decimal Sept = 7;
+            decimal Oct = 11;
+            decimal Nov = 9;
+            decimal Dec = 1;
+            new Chart(width: 600, height: 400, theme: ChartTheme.Blue)
+                .AddSeries(
+                chartType: "column",
+                xValue: new[] { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec" },
+                yValues: new[] { Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sept, Oct, Nov, Dec })
+                .AddTitle("Số lượng sản phẩm trong năm")
+                //.AddSeries("Defaulf", chartType: "Column", xValue: xValue, yValues: yValue)
+                .Write("bmp");
+            return null;
         }
 
         [HttpPost]
